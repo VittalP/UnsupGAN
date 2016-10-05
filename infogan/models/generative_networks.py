e5cebddd9df70c4c3129bf087567d0a5907aa1dd
Vittal <vittalp88@gmail.com>
2016-10-04 23:36:39 -0400

    replace name with scope

--- infogan/models/generative_networks.py
+++ infogan/models/generative_networks.py
@@ -6,7 +6,9 @@ class GenNetworks(object):
         print "__init__ - ing"
 
 class InfoGAN_mnist_net():
-    
+    def __init__(self):
+        self.gf_dim = 64
+        return
     def infoGAN_mnist_net(self, image_shape):
         image_size = image_shape[0]
         generator_template = \
@@ -23,4 +25,34 @@ class InfoGAN_mnist_net():
              apply(tf.nn.relu).
              custom_deconv2d([0] + list(image_shape), k_h=4, k_w=4).
              flatten())
+        return generator_template
+
+class dcgan_net():
+    def __init__(self):
+        self.gf_dim = 64
+        self.c_dim = 3
+        self.batch_size = 64
+
+    def dcgan_gen_net(self, image_shape):
+        s = image_shape[0]
+        s2, s4, s8, s16 = int(s/2), int(s/4), int(s/8), int(s/16)
+
+        generator_template = \
+            (pt.template("input").
+             custom_fully_connected(self.gf_dim*8*s16*s16, scope='g_h0_lin').
+             fc_batch_norm().
+             apply(tf.nn.relu).
+             reshape([-1, s16, s16, self.gf_dim * 8]).
+             custom_deconv2d([self.batch_size, s8, s8, self.gf_dim*4], name='g_h1').
+             conv_batch_norm().
+             apply(tf.nn.relu).
+             custom_deconv2d([self.batch_size, s4, s4, self.gf_dim*2], name='g_h2').
+             conv_batch_norm().
+             apply(tf.nn.relu).
+             custom_deconv2d([self.batch_size, s2, s2, self.gf_dim*1], name='g_h3').
+             conv_batch_norm().
+             apply(tf.nn.relu).
+             custom_deconv2d([self.batch_size, s, s, self.c_dim], name='g_h4').
+             apply(tf.nn.tanh))
+
         return generator_template
