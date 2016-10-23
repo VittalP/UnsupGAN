ce4eb5ab417d508d289e9b229826c7b5ad62779b
Vittal <vittalp88@gmail.com>
2016-10-23 13:26:36 -0400

    Lint some lines

--- infogan/models/generative_networks.py
+++ infogan/models/generative_networks.py
@@ -1,13 +1,16 @@
 import tensorflow as tf
 import prettytensor as pt
 
+
 class GenNetworks(object):
     def __init__(self):
         print "__init__ - ing"
 
+
 class InfoGAN_mnist_net():
     def __init__(self):
         return
+
     def infoGAN_mnist_net(self, image_shape):
         image_size = image_shape[0]
         generator_template = \
@@ -19,12 +22,13 @@ class InfoGAN_mnist_net():
              fc_batch_norm().
              apply(tf.nn.relu).
              reshape([-1, image_size / 4, image_size / 4, 128]).
-             custom_deconv2d([0, image_size / 2, image_size / 2, 64], k_h=4, k_w=4).
+             custom_deconv2d([0, image_size/2, image_size/2, 64], k_h=4, k_w=4).
              conv_batch_norm().
              apply(tf.nn.relu).
              custom_deconv2d([0] + list(image_shape), k_h=4, k_w=4).
              flatten())
         return generator_template
+
 
 class dcgan_net():
     def __init__(self):
@@ -39,7 +43,7 @@ class dcgan_net():
 
         if s == 32:
             self.k_h = self.k_w = 3
-            
+
         generator_template = \
             (pt.template("input").
              custom_fully_connected(self.gf_dim*8*s16*s16, scope='g_h0_lin').
