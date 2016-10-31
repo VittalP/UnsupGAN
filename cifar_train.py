38ad3d3acc189a83a14e2f06f3f8a6da3c030d64
Vittal <vittalp88@gmail.com>
2016-10-31 17:23:03 -0400

    do kmeans on learnt features

--- cifar_train.py
+++ cifar_train.py
@@ -26,7 +26,7 @@ discriminator_template = shared_template.custom_fully_connected(10)
 input_tensor = tf.placeholder(tf.float32, [batch_size] + [dataset.output_size, dataset.output_size, 3])
 label_tensor = tf.placeholder(tf.int64, [batch_size])
 
-d_features = shared_template.construct(input=input_tensor)
+d_feat = shared_template.construct(input=input_tensor)
 d_logits = discriminator_template.construct(input=input_tensor)
 d_prob = tf.nn.sigmoid(d_logits)
 
@@ -59,16 +59,49 @@ with tf.Session() as sess:
             summary_str = sess.run(summary_op, {input_tensor: x, label_tensor: labels})
             summary_writer.add_summary(summary_str, counter)
 
-            if counter % 500 == 0:
+            if counter % 1 == 0:
                 print "validating..."
                 pred_labels = np.array([], dtype=np.int16).reshape(0,)
+                pred_labels_kmeans = np.array([], dtype=np.int16).reshape(0,)
+                n_clusters = val_dataset.n_labels
                 labels = []
+
+                trainX = np.array([]).reshape(0, 0)
+
+                def pool_features(feat, pool_type='avg'):
+                    if pool_type == 'avg':
+                        feat = feat.mean(axis=(1, 2))
+                    if pool_type == 'max':
+                        feat = feat.mean(axis=(1, 2))
+                    return feat.reshape((feat.shape[0], feat.shape[-1]))
+
+                print "Getting all the training features."
+                for ii in range(val_dataset.batch_idx['train']):
+                    x, _ = val_dataset.next_batch(batch_size, split='train')
+                    d_features = sess.run(d_feat, {input_tensor: x})
+                    d_features = pool_features(d_features, pool_type='avg')
+                    if trainX.shape[0] == 0:  # Is empty
+                        trainX = d_features
+                    else:
+                        trainX = np.concatenate((trainX, d_features), axis=0)
+                print "Learning the clusters."
+                from sklearn.cluster import KMeans
+                kmeans = KMeans(n_clusters=n_clusters, init='k-means++').fit(trainX)
+
+                print "Extracting features from val set and predicting from it."
                 for ii in range(val_dataset.batch_idx['val']):
                     x, batch_labels = val_dataset.next_batch(batch_size=batch_size, split="val")
                     feed_dict = {input_tensor: x}
-                    pred_prob = sess.run(d_prob, {input_tensor: x})
+                    d_out = sess.run([d_features, d_prob], {input_tensor: x})
+                    d_features = d_out[0]
+                    pred_prob = d_out[1]
                     batch_pred_labels = np.argmax(pred_prob, axis=1)
                     pred_labels = np.concatenate((pred_labels, batch_pred_labels))
                     labels = labels + batch_labels
 
+                    d_features = pool_features(d_features, pool_type='avg')
+                    batch_pred_labels_kmeans = kmeans.predict(d_features)
+                    pred_labels_kmeans = np.concatenate((pred_labels_kmeans, batch_pred_labels_kmeans))
+
                 compute_cluster_scores(labels=np.asarray(labels), pred_labels=pred_labels, path=os.path.join('./scores.txt'))
+                compute_cluster_scores(labels=np.asarray(labels), pred_labels=pred_labels_kmeans, path=os.path.join('./scores_kmeans.txt'))
